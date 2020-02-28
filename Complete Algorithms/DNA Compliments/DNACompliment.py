c54965ed7fcf7c60a1a8dc6bb0b6b09cc54e214b
Steven Dea <36655621+srdea93@users.noreply.github.com>
2020-02-27 17:35:40 -0800

    Add files via upload

---
+++ Complete Algorithms/DNA Compliments/DNACompliment.py
@@ -0,0 +1,20 @@
+def DNA_compliment(text):
+    compliment = []
+    rev_text = text[::-1]
+    for letter in rev_text:
+        if letter == "A":
+            compliment.append("T")
+        elif letter == "T":
+            compliment.append("A")
+        elif letter == "G":
+            compliment.append("C")
+        elif letter == "C":
+            compliment.append("G")
+        else:
+            print("Invalid sequence")
+            return
+    compliment = "".join(compliment)
+    return compliment
+
+text = "AGGGGCCCTGTATAAATATAGGCCATATA"
+print(DNA_compliment(text))
